808e687c1a300f259044378f6a60c1ad86b1a512
Michael Saah <mike.saah@gmail.com>
2018-10-29 21:33:46 -0400

    forgot to include builders.py in last commit

---
+++ json_gen/builders.py
@@ -0,0 +1,113 @@
+import random, string
+
+# Building blocks for generators - not exposed to API
+
+class Sampler:
+    def __init__(self, values=None):
+        if values:
+            self.values = values
+    def __call__(self):
+        return random.sample(self.values, 1)[0]
+    def contains(self, x):
+        return x in self.values
+
+
+class WeightedSampler:
+    def __init__(self, values=None, weights=None):
+        if values:
+            self.values = values
+        if weights:
+            self.weights = weights
+    def __call__(self):
+        pass
+    def contains(self, x):
+        return x in self.values
+
+
+class IntegerBetween:
+    @staticmethod
+    def __call__(l, u):
+        if l > u:
+            raise ValueError('The lower bound must be less than or equal to the upper bound')
+        return random.sample(range(l, u+1), 1)[0]
+
+
+class FloatBetween:
+    @staticmethod
+    def __call__(l, u, k=None):
+        width = u - l
+        f =  random.random() * width + l
+        if k:
+            return round(f, k)
+        else:
+            return f
+
+
+class RandomChar(Sampler):
+    values = string.ascii_letters + string.digits
+
+
+class VosesAlias:
+    """
+    Vose's Alias is a variation of the alias algorithm for sampling
+    from an arbitrary given discrete probability distribution. To use,
+    simply pass a list of values (of any type) you'd like sampled, 
+    and a list of weights specifying the probability of sampling each 
+    value.
+
+    Given the nature of floating-point comparison, we leave it up 
+    to the user to make sure that the weights defines a 
+    probability distribution; that is, that they sums to one. The 
+    algorithm can handle some amount of numerical noise, so don't 
+    sweat this too hard.
+    """
+    def __init__(self, values, weights):
+        if len(values) != len(weights):
+            raise ValueError('Values and weights lack parity. \
+                Please pass the same number of values and weights.')
+
+        self.values = values
+        self.n = len(weights)
+
+        scaled_weights = list(map(lambda x: x*self.n, weights))
+        small = []
+        large = []
+
+        alias = [0]*self.n
+        prob = [0]*self.n
+
+        for i,p in enumerate(scaled_weights):
+            if p < 1:
+                small.append(i)
+            else:
+                large.append(i)
+
+        while large and small:
+            l = small.pop(0)
+            g = large.pop(0)
+            prob[l] = scaled_weights[l]
+            alias[l] = g
+            scaled_weights[g] = scaled_weights[g] + scaled_weights[l] - 1
+            if scaled_weights[g] < 1:
+                small.append(g)
+            else:
+                large.append(g)
+
+        while large:
+            g = large.pop(0)
+            prob[g] = 1
+       
+        while small: # large should be exhausted first, 
+                     # this catches numerical error
+            l = small.pop(0)
+            prob[l] = 1
+
+        self.alias = alias
+        self.prob = prob
+
+    def __call__(self):
+        i = random.sample(range(self.n), 1)[0]
+        if random.random() < self.prob[i]:
+            return self.values[i]
+        else:
+            return self.values[self.alias[i]]
